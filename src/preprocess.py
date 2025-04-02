660a586bbbf46fcf25582de71cbe57604a673ec0
techn0z <minhtrietamoled@gmail.com>
2025-04-02 21:49:57 +0700

    update

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,12 +3,12 @@ from data_lib import *
 
 
 data_loader = EcgDataLoader("database")
-ecgs = data_loader.load_raw_data(1, 2)
+ecgs = data_loader.load_raw_data()
 ecgs = np.transpose(ecgs, (0, 2, 1))
 
 ecgs = np.array([
    [MySignal.clean_ecg(e) for e in e12] for e12 in ecgs
 ])
 
-# ecgs = ecgs.astype(np.float32)
-# np.save(path.join("data", "ecgs"), ecgs)
+ecgs = ecgs.astype(np.float32)
+np.save(path.join("data", "ecgs"), ecgs)
